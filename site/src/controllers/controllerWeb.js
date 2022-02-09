347960fe08685011a36234349a60fc10f79ea114
damian0276 <damian0276@gmail.com>
2022-02-09 20:57:24 -0300

    agregue fotos

--- site/src/controllers/controllerWeb.js
+++ site/src/controllers/controllerWeb.js
@@ -7,8 +7,7 @@ const fs = require("fs");
 
 module.exports = {
     index : function(req,res){
-        res.sendFile(path.resolve(__dirname, "..", "views", "web", "index.ejs"));
-
+        res.render(path.resolve(__dirname, '..', 'views','web','index'))
     },
     
 
--- site/src/index.js
+++ site/src/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 //Para indicarle express la carpeta donde se encuentran los archivos estáticos
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 // Uso de EJS
-app.set("view engine","ejs");
+app.set('view engine','ejs');
 
 
 //Uso  a las rutas
@@ -24,7 +24,7 @@ app.use(webRoutes);
 
 
 // configuro el puerto por si el servidor ya me asigna uno//
-app.set('port', process.env.PORT || 3030);
+app.set('port', process.env.PORT || 3000);
 
 // Aca levanto el servidor 
 app.listen(app.get('port'), ()=> console.log('Servidor funcionando en el puerto',app.get('port')));
